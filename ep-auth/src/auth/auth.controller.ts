ec21271453be0bb84cac89327e09dabf4705b820
Sheron Jude <sheronjude4@gmail.com>
2026-01-21 23:58:22 +0200

    chore: login endpoint and types added

--- ep-auth/src/auth/auth.controller.ts
+++ ep-auth/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import type { LoginDto } from './dto/login.dto.ts';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+  @Post('/login')
+  login(@Body() loginDto: LoginDto): string {
+    return this.authService.login(loginDto);
+  }
+}
--- ep-auth/src/auth/auth.service.ts
+++ ep-auth/src/auth/auth.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  login(loginDto: LoginDto): string {
+    const { username } = loginDto;
+    return `User ${username} logged in successfully`;
+  }
+}
